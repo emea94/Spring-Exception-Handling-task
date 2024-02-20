8dac0462fda454b9f2cee5f430e7a369af35583e
emea94 <emea94@web.de>
2024-02-20 16:30:25 +0100

    add Test for AnimalController for Method getAnimalSpecies

---
+++ src/test/java/de/neuefische/springexceptionhandlingtask/AnimalControllerTest.java
@@ -0,0 +1,30 @@
+package de.neuefische.springexceptionhandlingtask;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AnimalControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getAnimalSpecies_whenSpeciesIsCat_ThenReturnExceptionHandling() throws Exception {
+        mvc.perform(get("/api/animals/cat")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+    }
+}
